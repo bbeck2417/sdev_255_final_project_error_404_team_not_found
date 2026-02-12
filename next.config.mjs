879f89cbca66789a2c11dffea1d6f501cb09e0e9
bbeck2417 <billys.beck@gmail.com>
2026-02-12 15:38:04 -0500

    added config for github pages to next.config.js file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/sdev_255_final_projecct_error_404_team_not_found",
+  assetPrefix: "/sdev_255_final_projecct_error_404_team_not_found",
   reactCompiler: true,
 };
 
