121884a3668a1ed5ce40e39e7159dd392b1c1fe9
bbeck2417 <billys.beck@gmail.com>
2026-02-14 21:10:46 -0500

    chore: correcting issues while converting from react to typescript

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,12 +2,10 @@ import { PrismaClient } from "../app/generated/prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// This is the object you will import in your components
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    // Tell the client to use the URL from your .env
-    datasourceUrl: process.env.DATABASE_URL,
+    log: ["query"],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
